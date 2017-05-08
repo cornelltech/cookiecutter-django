f16ee840d1570b46a5d483d11a35f4d2a273a363
Scot Spinner <sts59@cornell.edu>
2017-05-08 13:26:27 -0400

    adding restframework

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -1,19 +1,10 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.urlresolvers import reverse
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
 
-
-@python_2_unicode_compatible
-class User(AbstractUser):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
-
-    def __str__(self):
-        return self.username
-
-    def get_absolute_url(self):
-        return reverse('users:detail', kwargs={'username': self.username})
+class User(User):
+    """
+    Almost empty Users class
+    """
+    is_valid = models.BooleanField(default=False, blank=True)
